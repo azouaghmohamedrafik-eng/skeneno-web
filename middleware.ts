910b423a5e3f6f391aee09da37e63b18d5ca2b92
rafik <asia.hoceima@gmail.com>
2026-06-05 23:49:11 +0100

    web true

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const isVentaMode = true; // true = Modo Venta activo
+  const isVentaMode = false; // true = Modo Venta activo
 
   const { pathname } = request.nextUrl;
 
